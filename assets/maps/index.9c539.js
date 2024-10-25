b52ff7554bd560b2094fb9cd2e5935e64e508719
sengmu <66844703+sengmu@users.noreply.github.com>
2024-10-25 20:01:17 +0800

    Add files via upload

---
+++ assets/maps/index.9c539.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/maps",[],(function(){"use strict";return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/maps', 'chunks:///_virtual/maps'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
